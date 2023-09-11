a05999112006a86c52d54f9c9c41adf8d66ccf61
Ahmed Reza <ahmedrezashams4@gmail.com>
2023-09-11 23:55:42 +0600

    9-11

--- WindowsFormsApp1/Form4.Designer.cs
+++ WindowsFormsApp1/Form4.Designer.cs
@@ -50,6 +50,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(291, 245);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // comboBox1
